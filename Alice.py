7bc34e0c837dd4476de9b374fe529dc0717ee31c
Le Trong Anh Tu <letronganhtu@gmail.com>
2022-12-28 17:17:00 +0700

    update all - basically completed

--- Alice.py
+++ Alice.py
@@ -10,12 +10,16 @@ with open('parameter.txt', encoding = 'utf-8') as f:
 import random
 import socket
 import numpy as np
-
 from digitalSignature import RSAsign, RSAverify
 from encrypt_decrypt import encrypt, decrypt, matInvMod
  
 s = socket.socket()
 s.connect(("localhost", 1203))
+
+count_message = 0
+pA = 113658436785139662796223136980555068607462604577987509636788088080318546081514054104144409693326063583110947795205652085425805184552119018806609211242120470814388980461297179573847102907483713541247287395008519163649641393142474317297199404053596942261356796340996425471596388415961886902706565659923727624289
+qA = 160050402698988181905146140298688795269418737468199439444518303182550992095086837450584681179863645857391629240427926284717274826061590968333848421812794053900873953602679011201323898335266657577891434675457278660109652028758270302689559536597767355295287742730553336614733681243077527136655401631967487106987
+phiA = (pA - 1) * (qA - 1)
 
 def fast_pow_mod(a, b, p):
     res = 1
@@ -26,72 +30,78 @@ def fast_pow_mod(a, b, p):
         b = b // 2
     return res
 
-count_message = 0
-
 def setup_key(g, p):
+    # Setup private key
     sk_Alice = random.randint(2, p - 1)
     dlp_Alice = fast_pow_mod(g, sk_Alice, p)
+
     s.send(str(dlp_Alice).encode())
+
     dlp_Bob = int(s.recv(1024).decode())
+
     sk = fast_pow_mod(dlp_Bob, sk_Alice, p)
-    temp = sk
+
+    # Create key_matrix
     L = np.zeros((12, 12))
     U = np.zeros((12, 12))
 
     # Generate diag(L)
     for i in range(0, 12):
-        temp1 = temp % 100
-        temp = temp // 100
+        temp1 = sk % 100
+        sk = sk // 100
         while temp1 == 0:
-            temp1 = temp % 100
-            temp = temp // 100
+            temp1 = sk % 100
+            sk = sk // 100
         L[i][i] = temp1
 
-    # Generate diag(R)
+    # Generate diag(U)
     for i in range(0, 12):
-        temp1 = temp % 100
-        temp = temp // 100
+        temp1 = sk % 100
+        sk = sk // 100
         while temp1 == 0:
-            temp1 = temp % 100
-            temp = temp // 100
+            temp1 = sk % 100
+            sk = sk // 100
         U[i][i] = temp1
 
-    # Generate L, R -> L: i > j, R: i < j
+    # Generate L, R (L: i > j && R: i < j)
     for i in range(0, 12):
         for j in range(i + 1, 12):
-            U[i][j] = temp % 100
-            temp = temp // 100
-            if temp == 0: temp = 1
-            L[j][i] = temp % 100
-            temp = temp // 100
-            if temp == 0: temp = 101
+            U[i][j] = sk % 100
+            sk = sk // 100
+            if sk == 0: sk = 1
+            L[j][i] = sk % 100
+            sk = sk // 100
+            if sk == 0: sk = 101
 
     key_matrix = np.dot(L, U)
     for i in range(0, 12):
         for j in range(0, 12):
             key_matrix[i][j] %= 251
+
     return key_matrix
 
 sk_communicate = setup_key(g, p)
 sk_communicate = sk_communicate.astype(int)
 sk_inv = matInvMod(sk_communicate, 251)
 sk_inv = sk_inv % 251
-block_size = 12
+dA = pow(eA, -1, phiA)
+
 print(sk_communicate)
 
 while True:
     msg_inp = input("Alice: ")
     Alice_msg = encrypt(msg_inp, sk_communicate)
-    s.send(Alice_msg.encode(encoding='utf-8'))
-    s.send(str(RSAsign(msg_inp, d, n)).encode())
-    msg_inp = s.recv(1024).decode(encoding='utf-8')
-    sign = int(s.recv(1024).decode())
-    print("sign of message: ", sign)
+    s.send(Alice_msg.encode(encoding = 'utf-8'))
+    s.send(str(RSAsign(msg_inp, dA, nA)).encode())
+
+    msg_inp = s.recv(1024).decode(encoding = 'utf-8')
+    sign_B = int(s.recv(1024).decode())
     Bob_msg = decrypt(msg_inp, sk_inv)
-    if RSAverify(Bob_msg, sign, e, n):
+    if RSAverify(Bob_msg, sign_B, eB, nB):
         print("Bob:", Bob_msg)
     else:
-        print("message was changed")
+        print("Notification: Message is not belong to Bob")
+
     count_message += 2
     if count_message == 10:
         sk_communicate = setup_key(g, p)
--- Bob.py
+++ Bob.py
@@ -15,11 +15,15 @@ from encrypt_decrypt import encrypt, decrypt, matInvMod
 
 host = 'localhost'
 port = 1203
- 
 s = socket.socket()
 s.bind((host, port))
 s.listen(1)
 c, addr = s.accept()
+
+count_message = 0
+pB = 179728538306884079398457288348066998853352219039249437614727251447184351686624955454911993858991754503372836656800209148393382934261699922823944185325286359123651842263657000411821183956578227605909850733106370988323885078636229785024164089093242384827872271748469181412837289310958657973628429440885905292237
+qB = 134100871424245206782626629395231019933545892507830108674137289164362564793422418598265221112506337841272234726494910128486401814092625782372415463845746777117257199302532928137599901248609428017888868530802706435551627847656785891586772611020916107597543791601376633967071595969550122331247743592703682067187
+phiB = (pB - 1) * (qB - 1)
  
 def fast_pow_mod(a, b, p):
     res = 1
@@ -29,72 +33,79 @@ def fast_pow_mod(a, b, p):
         a = (a * a) % p
         b = b // 2
     return res
-
-count_message = 0
  
 def setup_key(g, p):
+    # Setup private key
     sk_Bob = random.randint(2, p - 1)
     dlp_Bob = fast_pow_mod(g, sk_Bob, p)
+
     dlp_Alice = int(c.recv(1024).decode())
+
     c.send(str(dlp_Bob).encode())
+
     sk = fast_pow_mod(dlp_Alice, sk_Bob, p)
-    temp = sk
+
+    # Create key_matrix
     L = np.zeros((12, 12))
     U = np.zeros((12, 12))
 
     # Generate diag(L)
     for i in range(0, 12):
-        temp1 = temp % 100
-        temp = temp // 100
+        temp1 = sk % 100
+        sk = sk // 100
         while temp1 == 0:
-            temp1 = temp % 100
-            temp = temp // 100
+            temp1 = sk % 100
+            sk = sk // 100
         L[i][i] = temp1
 
-    # Generate diag(R)
+    # Generate diag(U)
     for i in range(0, 12):
-        temp1 = temp % 100
-        temp = temp // 100
+        temp1 = sk % 100
+        sk = sk // 100
         while temp1 == 0:
-            temp1 = temp % 100
-            temp = temp // 100
+            temp1 = sk % 100
+            sk = sk // 100
         U[i][i] = temp1
 
-    # Generate L, R -> L: i > j, R: i < j
+   # Generate L, R (L: i > j && R: i < j)
     for i in range(0, 12):
         for j in range(i + 1, 12):
-            U[i][j] = temp % 100
-            temp = temp // 100
-            if temp == 0: temp = 1
-            L[j][i] = temp % 100
-            temp = temp // 100
-            if temp == 0: temp = 101
+            U[i][j] = sk % 100
+            sk = sk // 100
+            if sk == 0: sk = 1
+            L[j][i] = sk % 100
+            sk = sk // 100
+            if sk == 0: sk = 101
 
     key_matrix = np.dot(L, U)
     for i in range(0, 12):
         for j in range(0, 12):
             key_matrix[i][j] %= 251
+
     return key_matrix
  
 sk_communicate = setup_key(g, p)
 sk_communicate = sk_communicate.astype(int)
 sk_inv = matInvMod(sk_communicate, 251)
 sk_inv = sk_inv % 251
+dB = pow(eB, -1, phiB)
 
 print(sk_communicate)
+
 while True:
     msg_inp = c.recv(1024)
-    sign = int(c.recv(1024).decode())
-    Alice_msg = decrypt(msg_inp.decode(encoding='utf-8'), sk_inv)
-    print("sign of message: ", sign)
-    if RSAverify(Alice_msg, sign, e, n):
+    sign_A = int(c.recv(1024).decode())
+    Alice_msg = decrypt(msg_inp.decode(encoding = 'utf-8'), sk_inv)
+    if RSAverify(Alice_msg, sign_A, eA, nA):
         print("Alice:", Alice_msg)
     else:
-        print("message was changed")
+        print("Notification: Message is not belong to Alice")
+
     msg_inp = input("Bob: ")
     Bob_msg = encrypt(msg_inp, sk_communicate)
-    c.send(Bob_msg.encode(encoding='utf-8'))
-    c.send(str(RSAsign(msg_inp, d, n)).encode())
+    c.send(Bob_msg.encode(encoding = 'utf-8'))
+    c.send(str(RSAsign(msg_inp, dB, nB)).encode())
+
     count_message += 2
     if count_message == 10:
         sk_communicate = setup_key(g, p)
